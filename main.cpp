b7611d36da4be147606f89fed18087fcd4463b9e
Kolxein <Fomeni@rambler.ru>
2013-08-22 13:55:10 +0300

    Const

--- main.cpp
+++ main.cpp
@@ -5,24 +5,22 @@
 using namespace std;
 
 struct Point
-	{
+{
 	public:
 	int x;
 	int y;
-	};
+};
 const int FIELD_HEIGHT = 7;
 const int FIELD_WIDTH = 20;
 
-void draw(int board, Point obgect);
+void draw(const int& board, const Point& obgect);
 
-	
 int main()
 {
 	int board = 0;
-	
 	Point obgect;
-		obgect.x = 1;
-		obgect.y = 1;
+	 obgect.x = 1;
+	 obgect.y = 1;
 	draw(board,obgect);
 	cout<< board << "\n";
 	cout<< obgect.x << "\n";
@@ -34,24 +32,24 @@ int main()
 	return 0;
 }
 
-void draw(int board, Point obgect)
+void draw(const int& board, const Point& obgect)
 {
 	for (int counterH = 0; counterH < FIELD_HEIGHT; counterH++) 
 	{
 		for (int counterW = 0; counterW < FIELD_WIDTH; counterW++)
 		{
-		if (counterW == obgect.x && counterH == obgect.y)
-			{
-			cout << "*";
-			continue;
-			}
-		if (counterW == board && counterH == FIELD_HEIGHT - 2)
-			 {
-			 cout << "_";
-			 continue;
-			 }
-		cout << "-" ;
+			if (counterW == obgect.x && counterH == obgect.y)
+				{
+				cout << "*";
+				continue;
+				}
+			if (counterW == board && counterH == FIELD_HEIGHT - 2)
+				{
+				cout << "_";
+				continue;
+				}
+			cout << "-" ;
 		}
-	cout << "\n";
+		cout << "\n";
 	}
 }
