3de6d4d737db4f17d7bbf2fe858a0dd22fac1e91
Kolxein <Fomeni@rambler.ru>
2013-08-23 15:57:38 +0300

    Class

--- main.cpp
+++ main.cpp
@@ -7,55 +7,52 @@ using namespace std;
 class Point
 {
 public:
-    Point(int x, int y);
-    ~Point();
-    int x;
-    int y;
+	Point(int a, int b);
+	int x;
+	int y;
 private:
 };
 
-Point::Point(int x, int y)
-{
-}
-Point::~Point()
+Point::Point(int a, int b)
+	: x(a)
+	, y(b)
 {
 }
  
 const int FIELD_HEIGHT = 7;
 const int FIELD_WIDTH = 20;
 
-void draw(const int& board, const Point&);
+void draw(const int& board, const Point& obgect);
 
 int main()
 {
 	int board = 0;
 	Point obgect(1,1);
-	 
-	draw(board,obgect);
+	draw(board, obgect);
 	cout<< board << "\n";
-	cout<< "obgectX" << "\n";
-	cout<< "obgectY" << "\n";
+	cout<< obgect.x << "\n";
+	cout<< obgect.y << "\n";
 	cout<< "GAME OVER!!!!\n\n";
 	cout<< "Press ENTER to continue...";
 	//cin.get();
 	getch();
 	return 0;
 }
-void draw(const int& board, const Point&)
+void draw(const int& board, const Point& obgect)
 {
 	for (int counterH = 0; counterH < FIELD_HEIGHT; counterH++) 
 	{
 		for (int counterW = 0; counterW < FIELD_WIDTH; counterW++)
 		{
-			/*if (counterW == Point obgect(x) && counterH == Point obgect(y))
+			if (counterW == obgect.x && counterH == obgect.y)
 			{
-    			cout << "*";
-    			continue;
+				cout << "*";
+				continue;
 			}
-			*/if (counterW == board && counterH == FIELD_HEIGHT - 2)
+			if (counterW == board && counterH == FIELD_HEIGHT - 2)
 			{
-        		cout << "_";
-        		continue;
+				cout << "_";
+				continue;
 			}
 			cout << "-" ;
 		}
