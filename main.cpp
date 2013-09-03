7f83a6e3c9022fc255c59d53c88b612ce52c7708
Kolxein <Fomeni@rambler.ru>
2013-09-03 11:06:49 +0300

    int key = cin.get()

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
-#include <conio.h>
+//#include <conio.h>
+#include <stdlib.h>
 #include "point.h"
 #include "draw.h"
 
 using namespace std;
-void button(const int& board, const Point& obgect);
+void button(const int& board, const Point& obgect)
+{
+	bool exit = true;
+	while(exit)
+	{
+		int key = cin.get();
+		cout << key << endl;
+		//int key = 0;
+		key = cin.get();
+		switch (key)
+		{
+		case 39:					//right
+			system("cls");
+			draw(board, obgect);
+			cout << "right\n";
+			break;
+		case 37:					//left
+			system("cls");
+			draw(board, obgect);
+			cout << "left\n";
+			break;
+		case 10:
+			cout << "Entr\n";
+			exit = false;
+			break;
+		}
+	;}
+}
 
 int main()
 {
@@ -16,27 +44,5 @@ int main()
 	cin.get();
 	return 0;
 }
-void button(const int& board, const Point& obgect)
-{
-	bool exit = true;
-	while(exit)
-	{
-		int key = 0;
-		key = getch();
-		switch (key)
-		{
-		case 77:					//right
-			draw(board, obgect);
-			cout << "right\n";
-			break;
-		case 75:					//left
-			draw(board, obgect);
-			cout << "left\n";
-			break;
-		case 13:
-			cout << "Entr\n";
-			exit = false;
-			break;
-		}
-	;}
-}
+//void button(const int& board, const Point& obgect)
+
